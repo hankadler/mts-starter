20247fd26cd3e5876e6179e61459734d264cbcfe
Hank Adler <henryaguila1988@gmail.com>
2023-06-04 10:30:30 -0400

    change eslint array & object rules to allow one liners

--- src/lintMe.ts
+++ src/lintMe.ts
@@ -6,36 +6,18 @@ console.log(dirname(__filename));
 console.log(resolve(__dirname, ".."));
 console.log(join("foo", "bar"));
 
-const obj = {
-  one: 1,
-  two: 2,
-  three: 3
-};
+const obj = { one: 1, two: 2, three: 3 };
 
-const arr = [
-  1,
-  2,
-  3
-];
+const arr = [1, 2, 3];
 
 const { one, two } = obj;
 
 const [uno, dos] = arr;
 
 const func = (arg1: number | object, arg2?: number, arg3?: number, arg4?: number) => {
-  console.log({
-    arg1,
-    arg2,
-    arg3,
-    arg4
-  });
+  console.log({ arg1, arg2, arg3, arg4 });
 };
 
 func(one, two, uno, dos);
 
-func({
-  one,
-  two,
-  uno,
-  dos
-});
+func({ one, two, uno, dos });
