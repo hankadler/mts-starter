a2ee81546bce97a67d5974575058a2fdc12d532f
Hank Adler <henryaguila1988@gmail.com>
2023-06-11 10:52:00 -0400

    first commit to ava branch

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,25 +1,23 @@
 import sinon, { SinonSpy } from "sinon";
-import assert from "assert";
+import test, { ExecutionContext } from "ava";
 import greet from "../src/greet";
 
-describe("greet works", () => {
-  const mockConsole: Record<string, SinonSpy> = {};
+const mockConsole: Record<string, SinonSpy> = {};
 
-  before(() => {
-    mockConsole.spy = sinon.spy(console, "log");
-  });
+test.before(() => {
+  mockConsole.spy = sinon.spy(console, "log");
+});
 
-  after(() => {
-    mockConsole.spy.restore();
-  });
+test.after(() => {
+  mockConsole.spy.restore();
+});
 
-  it("logs \"Hello there!\" by default", () => {
-    greet();
-    assert(mockConsole.spy.calledWith("Hello there!"));
-  });
+test("logs \"Hello there!\" by default", (t: ExecutionContext) => {
+  greet();
+  t.assert(mockConsole.spy.calledWith("Hello there!"));
+});
 
-  it("logs \"Hello Ada!\" when name=Ada", () => {
-    greet("Ada");
-    assert(mockConsole.spy.calledWith("Hello Ada!"));
-  });
+test("logs \"Hello Ada!\" when name=Ada", (t: ExecutionContext) => {
+  greet("Ada");
+  t.assert(mockConsole.spy.calledWith("Hello Ada!"));
 });
