40a72ce437509a788668ab45b5a6be1847aacfeb
Henry Aguila <henry.aguila@outlook.com>
2023-07-14 23:00:18 -0400

    fix: use person constant

--- src/greet.ts
+++ src/greet.ts
@@ -2,8 +2,8 @@ import { Person } from "../type";
 
 function greet(name: string = "there", age = NaN) {
   const person: Person = { name, age };
-  const ageMsg = Number.isNaN(Number(age)) ? "" : ` You're ${age} years old!`;
-  console.log(`Hello ${name}!${ageMsg}`);
+  const ageMsg = Number.isNaN(Number(person.age)) ? "" : ` You're ${person.age} years old!`;
+  console.log(`Hello ${person.name}!${ageMsg}`);
 }
 
 export default greet;
